a7f4a406004145495e4b29778a73e1fa24800cf7
frunox <60527588+frunox@users.noreply.github.com>
2020-05-29 10:12:48 -0500

    remove default react route from server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // app.get('*', function (req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
 }
 
 // Add routes, both API and HTML.  Defaults to /routes/index.js

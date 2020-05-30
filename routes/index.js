03d04caa1cfff7cce53c3a4ae17437deb6a560ea
frunox <60527588+frunox@users.noreply.github.com>
2020-05-30 11:30:06 -0500

    remove default react route in server.js

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const router = require("express").Router();
 // Initialize API routes.
 const apiRoutes = require("./api");
 const utilRoutes = require("../utils");
-
+console.log('3. in routes/index.js', path.join(__dirname, "../client/public/index.html"))
 // NOTE: The only  routes I have seen is a "api", "html" and "util" routes.  Since we are using react, we do not need html routes?
-console.log('3. in backend routes/index.js')
 // Call API routes.
 router.use("/api", apiRoutes);
 

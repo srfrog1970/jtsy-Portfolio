e11d35f8f13fe5feffa928e89078ba2fbb0b3e74
frunox <60527588+frunox@users.noreply.github.com>
2020-05-30 09:55:01 -0500

    working server, react default route in routes/index.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  app.use(express.static("client/build"));
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  // app.get('*', function (req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
 }
 
 // Add routes, both API and HTML.  Defaults to /routes/index.js

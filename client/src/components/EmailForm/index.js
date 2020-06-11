398f2001cceb54016289691a8f31fa4ac6b9878c
frunox <60527588+frunox@users.noreply.github.com>
2020-06-11 10:13:21 -0500

    email address now added from state in Contact

--- client/src/components/EmailForm/index.js
+++ client/src/components/EmailForm/index.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button, Form, TextArea, Grid } from 'semantic-ui-react'
+import DevDataContext from "../../contexts/DevDataContext";
 // import "./emailform.css"
 
-const EmailForm = () => (
-    <Form action="mailto: jrcannon@msn.com" method="GET" target="_blank">
-        <Grid columns="1">
-            <Grid.Column width="6">
-                <Form.Field inline className='field'>
-                    <label className='label' for='subject'>Subject</label>
-                    <input className="input" name='subject' id='subject' type='text' placeholder='Subject...' />
-                </Form.Field>
-            </Grid.Column>
-            <Grid.Column width="12">
-                <Form.Field as="Segment" className='field'>
-                    <label className="label" for='body'>Message</label>
-                    <TextArea rows={3} className='textarea' name='body' id='body' placeholder='Message...' />
-                </Form.Field>
-            </Grid.Column>
-            <Grid.Column width="6">
-                <Button secondary type='submit'>Send</Button>
-            </Grid.Column>
-        </Grid>
-    </Form>
-)
+const EmailForm = () => {
+    const { devData } = useContext(DevDataContext);
+    console.log('in emailForm, email: ', devData.email);
+    return (
+        <Form action={"mailto: " + devData.email} method="GET" target="_blank">
+            <Grid columns="1">
+                <Grid.Column width="6">
+                    <Form.Field inline className='field'>
+                        <label className='label' htmlFor='subject'>Subject</label>
+                        <input className="input" name='subject' id='subject' type='text' placeholder='Subject...' />
+                    </Form.Field>
+                </Grid.Column>
+                <Grid.Column width="12">
+                    <Form.Field as="Segment" className='field'>
+                        <label className="label" htmlFor='body'>Message</label>
+                        <TextArea rows={3} className='textarea' name='body' id='body' placeholder='Message...' />
+                    </Form.Field>
+                </Grid.Column>
+                <Grid.Column width="6">
+                    <Button secondary type='submit'>Send</Button>
+                </Grid.Column>
+            </Grid>
+        </Form>
+    )
+}
 
 export default EmailForm

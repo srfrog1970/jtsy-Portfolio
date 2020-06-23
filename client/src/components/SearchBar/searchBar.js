c5e77603ac1e84f64efa4e661e8ac5f9eeccb499
frunox <60527588+frunox@users.noreply.github.com>
2020-06-23 08:53:36 -0500

    dev page search reset working

--- client/src/components/SearchBar/searchBar.js
+++ client/src/components/SearchBar/searchBar.js
@@ -1,28 +1,34 @@
+// import React, { useContext } from "react";
 import React from "react";
-import { Input, Button } from "semantic-ui-react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 import "./searchBar.css";
+// import DevDataContext from "../utils/DevDataContext";
 
 export default function searchBar({ resetSearch, handleInputChange }) {
   // const { devData, setDevData } = useContext(DevDataContext);
   // console.log(props);
-  let content = (
-    <div className="search" display="inline-block">
-      <form display="inline-block">
-        <Input
-          focus
-          placeholder="Search..."
-          className="searchBar"
+  return (
+    <div className="searchBar">
+      <InputGroup>
+        <FormControl
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="basic-addon2"
           onChange={(e) => handleInputChange(e)}
         />
-        <Button
-          type='reset'
-          className="clearButton"
-          onClick={(e) => resetSearch(e)}
-        >
-          Clear
+        <InputGroup.Append>
+          <Button
+            onClick={(e) => {
+              resetSearch(e);
+            }}
+            variant="dark"
+          >
+            Clear
         </Button>
-      </form>
+        </InputGroup.Append>
+      </InputGroup>
     </div>
-  )
-  return content
+  );
 }
